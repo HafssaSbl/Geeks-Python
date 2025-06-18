f08f79815356b109e0e8e32bdbeb73297bada470
HafssaSbl <hafssasabil@gmail.com>
2025-06-18 11:29:58 +0100

    first day

--- WEEK-1/DAY-1/Exercice-8.py
+++ WEEK-1/DAY-1/Exercice-8.py
@@ -1,23 +1,25 @@
+sandwich_orders = [
+    "Tuna sandwich", 
+    "Pastrami sandwich", 
+    "Avocado sandwich", 
+    "Pastrami sandwich", 
+    "Egg sandwich", 
+    "Chicken sandwich", 
+    "Pastrami sandwich"
+]
 
-sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", 
-                   "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
-
-print("Désolé, nous sommes en rupture de Pastrami sandwich.")
-
+print("Désolé, le pastrami est en rupture de stock.")
 
 while "Pastrami sandwich" in sandwich_orders:
     sandwich_orders.remove("Pastrami sandwich")
 
-
 finished_sandwiches = []
 
-
 while sandwich_orders:
-    sandwich = sandwich_orders.pop(0) 
+    sandwich = sandwich_orders.pop(0)
     print(f"Je prépare votre {sandwich.lower()}.")
     finished_sandwiches.append(sandwich)
 
-
-print("\n Tous les sandwiches ont été préparés :")
+print("\nListe des sandwichs préparés :")
 for sandwich in finished_sandwiches:
-    print(f"👉 I made your {sandwich.lower()}")
+    print(f"J'ai préparé votre {sandwich.lower()}.")
